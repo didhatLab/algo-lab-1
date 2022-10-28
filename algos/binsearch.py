37e15d233c418d101615da2148ddd746426be5e3
Daniil Solynin <didhat82@gmail.com>
2022-10-28 16:25:27 +0300

    add algos

---
+++ algos/binsearch.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def get_target_or_new_column_position(row: List[int], left: int, right: int, target: int):
+    while right > left:
+        mid = (right + left) // 2
+        if row[mid] == target:
+            return mid
+        elif row[mid] > target:
+            right = mid
+        elif row[mid] < target:
+            left = mid
+    return right - 1
+
+
+def bin_search(matrix: List[List[int]], target_number: int):
+    row_index = 0
+    column_index = len(matrix[0]) - 1
+
+    while row_index < len(matrix) and column_index >= 0:
+        if matrix[row_index][column_index] == target_number:
+            return True
+        elif matrix[row_index][column_index] < target_number:
+            row_index += 1
+        elif matrix[row_index][column_index] > target_number:
+            column_index = get_target_or_new_column_position(matrix[row_index], 0, column_index, target_number)
+    return False
---
+++ algos/exp.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+def find_target_or_get_new_column_position(row: List[int], left: int, right: int, target: int):
+    while right > left:
+        mid = (right + left) // 2
+        if row[mid] == target:
+            return mid
+        elif row[mid] > target:
+            right = mid
+        elif row[mid] < target:
+            left = mid
+    return right - 1
+
+
+def find_bounds_for_bin(row: List[int], left: int, right: int, target: int) -> [int, int]:
+    bound = 2
+
+    while right >= bound and row[left + bound] < target:
+        bound *= 2
+    return bound, right
+
+
+def exp_search(matrix: List[List[int]], target: int):
+    row_index = 0
+    column_index = len(matrix[0]) - 1
+
+    while row_index < len(matrix) and column_index >= 0:
+        if matrix[row_index][column_index] == target:
+            return True
+
+        elif matrix[row_index][column_index] < target:
+            row_index += 1
+
+        elif matrix[row_index][column_index] > target:
+            left, right = find_bounds_for_bin(matrix[row_index], 0, column_index, target)
+
+            column_index = find_target_or_get_new_column_position(matrix[row_index], left, right, target)
+    return False
