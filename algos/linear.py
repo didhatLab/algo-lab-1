6f45fabe6bebc3b5ffbdfe7f2adf3da42361327b
Daniil Solynin <didhat82@gmail.com>
2022-10-28 14:14:11 +0300

    add folder for tests

---
+++ algos/linear.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def linear_search(matrix: List[List[int]], target_number: int):
+    row_index = 0
+    column_index = len(matrix[0]) - 1
+
+    while row_index < len(matrix) and column_index >= 0:
+        if matrix[row_index][column_index] == target_number:
+            return True
+        if matrix[row_index][column_index] > target_number:
+            row_index += 1
+        elif matrix[row_index][column_index] < target_number:
+            row_index -= 1
+    return False
